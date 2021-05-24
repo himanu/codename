19b4439b01df9ebb894194732f1fc15a134d5c64
himanu <hy287719@gmail.com>
2021-05-24 18:09:32 +0530

    Desgined some webPage

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import App from './App.svelte';
-
+import App1 from './App1.svelte';
+import App2 from './App2.svelte';
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+	target: document.body
 });
 
 export default app;
