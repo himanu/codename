23817df4a8383915196fdab85459376d43275ee1
Asim Aashish <asimaashish1234@gmail.com>
2021-07-31 13:23:13 +0530

    add deploy scripts to codename

--- src/database.js
+++ src/database.js
@@ -4,13 +4,13 @@ import 'firebase/functions';
 import { getGameSessionId, getParams,defaultWordsArray,duetWordsArray } from "./utils";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkHeZ4jd3yxaEV7oP23CV3MXTdohs6btY",
-    authDomain: "da-cn-dev.firebaseapp.com",
-    databaseURL: "https://da-cn-dev-default-rtdb.firebaseio.com",
-    projectId: "da-cn-dev",
-    storageBucket: "da-cn-dev.appspot.com",
-    messagingSenderId: "50285255069",
-    appId: "1:50285255069:web:2c3a709277dbeb781b7774"
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
